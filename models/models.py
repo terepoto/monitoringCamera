c2a5837ac87cf4f5ff9ad4d1b433d7efd97f7a92
terepoto <675135466@qq,com>
2023-06-06 13:20:43 +0900

    モデル更新

--- models/models.py
+++ models/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
 
-# Create your tests here.
+class Faces(models.Model):
+    rekognition_face_id = models.CharField(max_length=255)
+    face_img = models.CharField(max_length=255)
+
+
+class Position_History(models.Model):
+    face_id = models.CharField(max_length=255)
+    camera_id = models.CharField(max_length=255)
+    position_x = models.CharField(max_length=255)
+    position_y = models.CharField(max_length=255)
+
+
+class Cameras(models.Model):
+    ip = models.CharField(max_length=255)
+    room_id = models.CharField(max_length=255)
+
+
+class Rooms(models.Model):
+    name = models.CharField(max_length=255)
